1ce2d4bb91779d652dd0e2baec96a0977277248e
marvinalmeida <marvin.almeida31@gmail.com>
2024-02-07 17:45:07 -0300

    começando logica do projeto

---
+++ app.js
@@ -0,0 +1,8 @@
+function criptografar() {
+    const textArea = document.querySelector("textarea").value
+    return console.log(textArea)
+
+    // const shadowArea = document.getElementsByClassName("remove-area")
+    // shadowArea.classList.add('')
+    // console.log(shadowArea)
+}
