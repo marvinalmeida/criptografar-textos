c3d74c45d528be19f7c2c3ae56c4afa10ed0bcc3
marvinalmeida <marvin.almeida31@gmail.com>
2024-03-19 21:01:46 -0300

    finalizando projeto

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-function criptografar() {
-    const textArea = document.querySelector("textarea").value
-    return console.log(textArea)
+const textoArea = document.querySelector("#textoArea");
 
-    // const shadowArea = document.getElementsByClassName("remove-area")
-    // shadowArea.classList.add('')
-    // console.log(shadowArea)
+const mostrarResultado = document.querySelector("#resultado")
+
+
+function criptografarTexto () {
+const texto = textoArea.value.replace(/e/g, "enter").replace(/i/g, "ilos").replace(/a/g, "oi").replace(/o/g, "ober").replace(/u/g, "umat")
+
+document.getElementById('resultado').innerHTML = '<div readonly class="shadow-resultado">' + texto + 
+'</div>' + '<button class="criptografar" id="copiar" onclick="copiar()">Copiar</button>'
+   
 }
+
+function descriptografar () {
+    const texto = textoArea.value.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+    
+    document.getElementById('resultado').innerHTML = '<div readonly class="shadow-resultado">' + texto + 
+    '</div>' + '<button class="criptografar" id="copiar" onclick="copiar()">Copiar</button>'
+       
+    }
+
+
+function copiar () {
+    var textoCop = document.getElementById('textoArea');
+  
+    textoCop.select();
+    document.execCommand('copy');
+    alert("Texto copiado.");
+}
+
+
